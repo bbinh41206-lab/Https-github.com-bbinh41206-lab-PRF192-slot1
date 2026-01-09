75a1b1345eaf5cee171959d6c49badea7ba284e2
Hồ Như Bình <bbinh41206@gmail.com>
2026-01-09 10:14:12 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+	char hoTen[]= "HoNhuBinh";
+	int tuoi = 18;
+    printf("Ho ten: %s \n",hoTen);
+    printf("Tuoi: %d",tuoi);
+    return 0; // Bao hieu chuong trinh da ket thuc 
+}
+
